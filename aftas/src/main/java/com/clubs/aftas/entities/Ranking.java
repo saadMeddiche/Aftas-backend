5a25d7f55ac1132016c4571bc59ba0f5cf972cbc
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 14:28:54 +0100

    Hibernate Validation For Ranking

--- aftas/src/main/java/com/clubs/aftas/entities/Ranking.java
+++ aftas/src/main/java/com/clubs/aftas/entities/Ranking.java
@@ -2,6 +2,9 @@ package com.clubs.aftas.entities;
 
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 @Entity
@@ -10,15 +13,21 @@ import lombok.*;
 @AllArgsConstructor
 public class Ranking extends BaseEntity {
 
+    @Positive(message = "Rank must be positive")
+    @NotNull(message = "Rank cannot be null")
     private Integer rank;
 
+    @PositiveOrZero(message = "Score must be positive")
+    @NotNull(message = "Score cannot be null")
     private Integer Score;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
+    @NotNull(message = "Member cannot be null")
     private Member member;
 
     @ManyToOne
     @JoinColumn(name = "competition_id")
+    @NotNull(message = "Competition cannot be null")
     private Competition competition;
 }
