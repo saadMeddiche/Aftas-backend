dc456284c81623afc6571c1c1c8574dfc7aebbc1
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 20:50:22 +0100

    Add @ Valid to CompetitionController.createCompetition

--- aftas/src/main/java/com/clubs/aftas/controllers/CompetitionController.java
+++ aftas/src/main/java/com/clubs/aftas/controllers/CompetitionController.java
@@ -3,6 +3,7 @@ package com.clubs.aftas.controllers;
 import com.clubs.aftas.dtos.competition.requests.CompetitionAddRequest;
 import com.clubs.aftas.entities.Competition;
 import com.clubs.aftas.services.CompetitionService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +20,7 @@ public class CompetitionController {
     private final CompetitionService competitionService;
 
     @PostMapping()
-    public ResponseEntity<?> createCompetition(@RequestBody CompetitionAddRequest competitionAddRequest) {
+    public ResponseEntity<?> createCompetition(@Valid @RequestBody CompetitionAddRequest competitionAddRequest) {
         Competition addedCompetition = competitionService.createCompetition(competitionAddRequest);
         return new ResponseEntity<>(addedCompetition, HttpStatus.CREATED);
     }
