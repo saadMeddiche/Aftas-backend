0597bdefaec221d1ca8a78b06fe5c1cb4d9c5ce1
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 14:39:47 +0100

    Hibernate Validation For Fish

--- aftas/src/main/java/com/clubs/aftas/entities/Fish.java
+++ aftas/src/main/java/com/clubs/aftas/entities/Fish.java
@@ -1,6 +1,10 @@
 package com.clubs.aftas.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Entity
@@ -10,11 +14,17 @@ import lombok.*;
 public class Fish extends BaseEntity {
 
     @Column(unique = true)
+    @NotNull(message = "Name cannot be null")
+    @NotBlank(message = "Name cannot be blank")
+    @Pattern(regexp = "^[a-zA-Z]+$", message = "Name must only contain letters")
     private String name;
 
+    @NotNull(message = "Average weight cannot be null")
+    @Positive(message = "Average weight must be positive")
     private Double averageWeight;
 
     // Many instances of Fish Class can be associated with one level
     @ManyToOne
+    @NotNull(message = "Level cannot be null")
     private Level level;
 }
