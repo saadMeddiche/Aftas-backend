f5277994a719c7847784c362048db36ee2e8fe7a
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-09 00:46:37 +0100

    And Handlers when parsing IdentityDocument , and localDate

--- aftas/src/main/java/com/clubs/aftas/handlingExceptions/ExceptionHandlerFactory.java
+++ aftas/src/main/java/com/clubs/aftas/handlingExceptions/ExceptionHandlerFactory.java
@@ -4,11 +4,13 @@ import com.clubs.aftas.handlingExceptions.costumExceptions.AlreadyExistsExceptio
 import com.clubs.aftas.handlingExceptions.costumExceptions.DateValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.format.DateTimeParseException;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -25,6 +27,20 @@ public class ExceptionHandlerFactory {
     @ExceptionHandler(DateValidationException.class)
     public ResponseEntity<?> handleDateValidationException(DateValidationException exception) {
         return new ResponseEntity<>(exception.getError() , HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleDHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+
+        if(exception.getMessage().contains("not one of the values accepted for Enum class: [CIN, PASSPORT, CARTE_RESIDENCE]")){
+            return new ResponseEntity<>("Invalid identityDocument value it shoudld be [CIN ,PASSPORT,CARTE_RESIDENCE]" , HttpStatus.BAD_REQUEST);
+        }
+
+        if(exception.getMessage().contains("Failed to deserialize java.time.LocalDate")){
+            return new ResponseEntity<>("Respect the format YYYY-MM-DD" , HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(exception.getMessage() , HttpStatus.BAD_REQUEST);
     }
 
     // Thanks To Yassine Sahyane
