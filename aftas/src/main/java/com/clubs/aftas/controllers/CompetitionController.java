f6e2b3ee9bb1be400dea9ffbd6a04163db3f92f9
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 20:19:05 +0100

    Create of CompetitionController.createCompetition() (Post)

---
+++ aftas/src/main/java/com/clubs/aftas/controllers/CompetitionController.java
@@ -0,0 +1,26 @@
+package com.clubs.aftas.controllers;
+
+import com.clubs.aftas.dtos.competition.requests.CompetitionAddRequest;
+import com.clubs.aftas.entities.Competition;
+import com.clubs.aftas.services.CompetitionService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/V1/competitions")
+@AllArgsConstructor
+public class CompetitionController {
+
+    private final CompetitionService competitionService;
+
+    @PostMapping()
+    public ResponseEntity<?> createCompetition(@RequestBody CompetitionAddRequest competitionAddRequest) {
+        Competition addedCompetition = competitionService.createCompetition(competitionAddRequest);
+        return new ResponseEntity<>(addedCompetition, HttpStatus.CREATED);
+    }
+}
