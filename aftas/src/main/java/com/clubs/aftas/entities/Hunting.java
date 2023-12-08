0d51b246cf29bebe37862d9c26387f52a42e075b
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 14:38:26 +0100

    Hibernate Validation For Hunting

--- aftas/src/main/java/com/clubs/aftas/entities/Hunting.java
+++ aftas/src/main/java/com/clubs/aftas/entities/Hunting.java
@@ -2,6 +2,8 @@ package com.clubs.aftas.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 @Entity
@@ -10,14 +12,19 @@ import lombok.*;
 @AllArgsConstructor
 public class Hunting extends BaseEntity {
 
+    @PositiveOrZero(message = "Number of fish must be positive")
+    @NotNull(message = "Number of fish cannot be null")
     private Integer numberOfFish;
 
     @ManyToOne
+    @NotNull(message = "Fish cannot be null")
     private Fish fish;
 
     @ManyToOne
+    @NotNull(message = "Member cannot be null")
     private Member member;
 
     @ManyToOne
+    @NotNull(message = "Competition cannot be null")
     private Competition competition;
 }
