7a41339f339fb7bd87cddbcf4e2a45506752ffb9
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 09:59:36 +0100

    Create Level Entity

---
+++ aftas/src/main/java/com/clubs/aftas/entities/Level.java
@@ -0,0 +1,13 @@
+package com.clubs.aftas.entities;
+
+import lombok.Data;
+
+@Data
+public class Level {
+
+    private Integer code;
+
+    private String description;
+
+    private Integer points;
+}
