143250f7d460efa4ed7f8bb540fa572cdb4e62fa
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 20:33:21 +0100

    Import ExceptionHandlerFactory.handleDateMethodArgumentNotValidException()

--- aftas/src/main/java/com/clubs/aftas/handlingExceptions/ExceptionHandlerFactory.java
+++ aftas/src/main/java/com/clubs/aftas/handlingExceptions/ExceptionHandlerFactory.java
@@ -4,9 +4,14 @@ import com.clubs.aftas.handlingExceptions.costumExceptions.AlreadyExistsExceptio
 import com.clubs.aftas.handlingExceptions.costumExceptions.DateValidationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @ControllerAdvice
@@ -20,5 +25,15 @@ public class ExceptionHandlerFactory {
     @ExceptionHandler(DateValidationException.class)
     public ResponseEntity<?> handleDateValidationException(DateValidationException exception) {
         return new ResponseEntity<>(exception.getError() , HttpStatus.BAD_REQUEST);
+    }
+
+    // Thanks To Yassine Sahyane
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleDateMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        Map<String, List<String>> errors = exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .collect(Collectors.groupingBy(FieldError::getField, Collectors.mapping(FieldError::getDefaultMessage, Collectors.toList())));
+        return new ResponseEntity<>(errors , HttpStatus.BAD_REQUEST);
     }
 }
