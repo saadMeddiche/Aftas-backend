7329f00a82962c1bb51777e8d9972b855b9188bf
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 15:33:15 +0100

    Create CompetitionServiceImpl (Just The Signatures without no content)

---
+++ aftas/src/main/java/com/clubs/aftas/services/implementations/CompetitionServiceImpl.java
@@ -0,0 +1,40 @@
+package com.clubs.aftas.services.implementations;
+
+import com.clubs.aftas.entities.Competition;
+import com.clubs.aftas.entities.Member;
+import com.clubs.aftas.entities.Ranking;
+import com.clubs.aftas.services.CompetitionService;
+
+import java.util.List;
+
+public class CompetitionServiceImpl implements CompetitionService {
+    @Override
+    public List<Competition> getAllCompetitions() {
+        return null;
+    }
+
+    @Override
+    public Competition getCompetitionById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Member> getParticipants(Competition competition) {
+        return null;
+    }
+
+    @Override
+    public List<Ranking> getRankings(Competition competition) {
+        return null;
+    }
+
+    @Override
+    public Competition createCompetition(Competition competition) {
+        return null;
+    }
+
+    @Override
+    public Competition updateCompetition(Competition competition) {
+        return null;
+    }
+}
