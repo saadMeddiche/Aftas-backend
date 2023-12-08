6a9765e0d9a40dd8e364a3c9846c13fa1c24b1f2
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 14:43:01 +0100

    Hibernate Validation For Competition

--- aftas/src/main/java/com/clubs/aftas/entities/Competition.java
+++ aftas/src/main/java/com/clubs/aftas/entities/Competition.java
@@ -1,6 +1,9 @@
 package com.clubs.aftas.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -16,22 +19,34 @@ import java.util.List;
 public class Competition extends BaseEntity {
 
     @Column(unique = true)
+    @NotNull(message = "Code cannot be null")
+    @NotBlank(message = "Code cannot be blank")
     private String code;
 
     @Column(unique = true)
+    @NotNull(message = "Name cannot be null")
     private LocalDate date;
 
+    @NotNull(message = "Start time cannot be null")
     private LocalTime startTime;
 
+    @NotNull(message = "End time cannot be null")
     private LocalTime endTime;
 
+    @NotNull(message = "Number of participants cannot be null")
+    @PositiveOrZero(message = "Number of participants must be positive")
     private Integer numberOfParticipants;
 
+    @NotNull(message = "Location cannot be null")
+    @NotBlank(message = "Location cannot be blank")
     private String location;
 
+    @NotNull(message = "Amount cannot be null")
+    @PositiveOrZero(message = "Amount must be positive")
     private Double amount;
 
     @OneToMany(mappedBy = "competition")
+    @NotNull(message = "Rankings cannot be null")
     private List<Ranking> rankings;
 
 }
