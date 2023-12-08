22c784e5a94fb1c48f70f123a1a8f811a20cefe1
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-08 15:27:22 +0100

    Create CompetitionRepository.findAllPagination() And CompetitionService

--- aftas/src/main/java/com/clubs/aftas/repositories/CompetitionRepository.java
+++ aftas/src/main/java/com/clubs/aftas/repositories/CompetitionRepository.java
@@ -4,6 +4,11 @@ import com.clubs.aftas.entities.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
 @Repository
 public interface CompetitionRepository extends JpaRepository<Competition, Long> {
+
+    List<Competition> findAllPagination(Long id , Pageable pageable);
 }
--- aftas/src/main/java/com/clubs/aftas/services/CompetitionService.java
+++ aftas/src/main/java/com/clubs/aftas/services/CompetitionService.java
@@ -1,6 +1,25 @@
 package com.clubs.aftas.services;
 
-public class CompetitionService {
+
+import com.clubs.aftas.entities.Competition;
+import com.clubs.aftas.entities.Member;
+import com.clubs.aftas.entities.Ranking;
+
+import java.util.List;
+
+public interface CompetitionService {
+
+    public List<Competition> getAllCompetitions();
+
+    public Competition getCompetitionById(Long id);
+
+    public List<Member> getParticipants(Competition competition);
+
+    public List<Ranking> getRankings(Competition competition);
+
+    public Competition createCompetition(Competition competition);
+
+    public Competition updateCompetition(Competition competition);
 
 
 }
