6668468e5427a4074b93014ef5c7ff48729e415c
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-10 18:44:33 +0100

    Create LevelRepositroy

---
+++ aftas/src/main/java/com/clubs/aftas/repositories/LevelRepository.java
@@ -0,0 +1,7 @@
+package com.clubs.aftas.repositories;
+
+import com.clubs.aftas.entities.Level;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LevelRepository extends JpaRepository<Level, Long> {
+}
