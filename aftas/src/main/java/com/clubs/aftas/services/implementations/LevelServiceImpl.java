c64fd8a485700d1498fd4846077744b8b91c7a98
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-10 20:39:01 +0100

    pagination and findby id for levelservice

--- aftas/src/main/java/com/clubs/aftas/services/implementations/LevelServiceImpl.java
+++ aftas/src/main/java/com/clubs/aftas/services/implementations/LevelServiceImpl.java
@@ -1,35 +1,61 @@
 package com.clubs.aftas.services.implementations;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+import com.clubs.aftas.dtos.level.requests.LevelRequest;
 import com.clubs.aftas.entities.Level;
+import com.clubs.aftas.handlingExceptions.costumExceptions.DoNotExistException;
+import com.clubs.aftas.handlingExceptions.costumExceptions.EmptyException;
+
+import com.clubs.aftas.services.BaseService;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.clubs.aftas.repositories.LevelRepository;
 import com.clubs.aftas.services.LevelService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 @Service
-@AllArgsConstructor
-public class LevelServiceImpl implements LevelService {
+
+public class LevelServiceImpl extends BaseService<Level, Long> implements LevelService {
 
     private final LevelRepository levelRepository;
+
+    public LevelServiceImpl(LevelRepository levelRepository){
+        super(levelRepository , Level.class);
+        this.levelRepository = levelRepository;
+    }
+
     @Override
     public List<Level> getAllLevels() {
-        return null;
+        return getAllEntities();
     }
+
+    @Override
+    public Page<Level> getAllLevelsWithPagination(Pageable pageable){
+        return getAllEntitiesWithPagination(pageable);
+    }
+
     @Override
     public Level getLevelById(Long id) {
+        return getEntityById(id);
+    }
+
+
+    @Override
+    public Level createLevel(LevelRequest levelRequest) {
         return null;
     }
 
     @Override
-    public Level addLevel(Level level) {
+    public Level updateLevel(LevelRequest level, Long levelId) {
         return null;
     }
 
