8ea0fd23411db06ef741f4d657e32bcecdd92e6b
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-10 20:39:17 +0100

    update the interface of level

--- aftas/src/main/java/com/clubs/aftas/services/LevelService.java
+++ aftas/src/main/java/com/clubs/aftas/services/LevelService.java
@@ -1,16 +1,27 @@
 package com.clubs.aftas.services;
 
+import com.clubs.aftas.dtos.level.requests.LevelRequest;
+import com.clubs.aftas.dtos.level.requests.LevelRequest;
 import com.clubs.aftas.entities.Level;
+import com.clubs.aftas.entities.Level;
+import com.clubs.aftas.entities.Member;
+import com.clubs.aftas.entities.Ranking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface LevelService {
 
-        List<Level> getAllLevels();
+        public List<Level> getAllLevels();
 
-        Level getLevelById(Long id);
+        public Page<Level> getAllLevelsWithPagination(Pageable pageable);
 
-        Level addLevel(Level level);
+        public Level getLevelById(Long id);
+
+        public Level createLevel(LevelRequest levelRequest);
+
+        public Level updateLevel(LevelRequest level , Long levelId);
 
         void createDefaultLevels();
 
