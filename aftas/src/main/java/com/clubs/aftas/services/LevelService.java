c6ce93b9bfcbc866d194977bc6cf2db771b1c38f
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-10 18:45:36 +0100

    Create LevelService interface

---
+++ aftas/src/main/java/com/clubs/aftas/services/LevelService.java
@@ -0,0 +1,17 @@
+package com.clubs.aftas.services;
+
+import com.clubs.aftas.entities.Level;
+
+import java.util.List;
+
+public interface LevelService {
+
+        List<Level> getAllLevels();
+
+        Level getLevelById(Long id);
+
+        Level addLevel(Level level);
+
+        void initializeLevels();
+
+}
