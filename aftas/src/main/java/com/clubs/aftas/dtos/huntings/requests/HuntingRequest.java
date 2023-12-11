1ef921c73bca7e97733332aee06d3fafdef259b1
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-11 16:06:34 +0100

    change the HuntingRequest attributes

--- aftas/src/main/java/com/clubs/aftas/dtos/huntings/requests/HuntingRequest.java
+++ aftas/src/main/java/com/clubs/aftas/dtos/huntings/requests/HuntingRequest.java
@@ -1,14 +1,9 @@
 package com.clubs.aftas.dtos.huntings.requests;
 
-import com.clubs.aftas.entities.Competition;
-import com.clubs.aftas.entities.Fish;
-import com.clubs.aftas.entities.Member;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+
+import jakarta.validation.constraints.Positive;
 import lombok.*;
 
 @Getter
@@ -18,16 +13,18 @@ import lombok.*;
 @AllArgsConstructor
 public class HuntingRequest {
 
-    @PositiveOrZero(message = "Number of fish must be positive")
-    @NotNull(message = "Number of fish cannot be null")
-    private Integer numberOfFish;
+    @NotNull(message = "Fish id cannot be null")
+    private Long fishId;
 
-    @NotNull(message = "Fish cannot be null")
-    private Fish fish;
+    @NotNull(message = "Weight cannot be null")
+    @Positive(message = "Weight must be positive")
+    @Max(value = 20000, message = "Weight must be less than or equal to 20000")
+    private Double weightOfHuntedFish;
 
-    @NotNull(message = "Member cannot be null")
-    private Member member;
+    @NotNull(message = "Member id cannot be null")
+    private Long memberId;
 
-    @NotNull(message = "Competition cannot be null")
-    private Competition competition;
+    @NotNull(message = "Competition id cannot be null")
+    private Long competitionId;
+
 }
