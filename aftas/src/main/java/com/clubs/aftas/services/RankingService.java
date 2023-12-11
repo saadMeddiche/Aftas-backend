8a2d47279a872835a924e1c27449f053ee308c19
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-11 18:03:06 +0100

    Create rankingserviceinterface

---
+++ aftas/src/main/java/com/clubs/aftas/services/RankingService.java
@@ -0,0 +1,20 @@
+package com.clubs.aftas.services;
+
+import com.clubs.aftas.entities.Ranking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RankingService {
+
+    public List<Ranking> getAllRankings();
+
+    public Page<Ranking> getAllRankingsWithPagination(Pageable pageable);
+
+    public Ranking getRankingById(Long id);
+
+    public Ranking createRanking(Ranking ranking);
+
+}
