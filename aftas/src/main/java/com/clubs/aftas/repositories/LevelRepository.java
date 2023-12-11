651095376891776b3a3facaa3f3115be8c28bfd1
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-11 09:59:37 +0100

    Create Level Repository

--- aftas/src/main/java/com/clubs/aftas/repositories/LevelRepository.java
+++ aftas/src/main/java/com/clubs/aftas/repositories/LevelRepository.java
@@ -3,5 +3,13 @@ package com.clubs.aftas.repositories;
 import com.clubs.aftas.entities.Level;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface LevelRepository extends JpaRepository<Level, Long> {
+
+    Optional<Level> findByCode(Integer code); //
+
+    Optional<Level> findByPoints(Integer points);
+
+     Optional<Level> findTopByOrderByIdDesc();
 }
