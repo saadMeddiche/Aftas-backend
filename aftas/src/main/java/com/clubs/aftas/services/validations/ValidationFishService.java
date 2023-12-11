f4d9bce67f79c441c140074147f458620e19a39e
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-11 11:20:49 +0100

    add ValidationFishService

---
+++ aftas/src/main/java/com/clubs/aftas/services/validations/ValidationFishService.java
@@ -0,0 +1,42 @@
+package com.clubs.aftas.services.validations;
+
+import com.clubs.aftas.entities.Fish;
+import com.clubs.aftas.handlingExceptions.costumExceptions.AlreadyExistsException;
+import com.clubs.aftas.repositories.FishRepository;
+import lombok.AllArgsConstructor;
+
+import java.util.Optional;
+
+@AllArgsConstructor
+public class ValidationFishService {
+
+    private final FishRepository fishRepository;
+
+    public void validateFishWhenCreating(Fish fish) {
+        throwExceptionIfFishNameIsAlreadyExists(fish.getName());
+    }
+
+    public void validateFishWhenUpdating(Fish fish, Long fishId) {
+        throwExceptionIfFishNameIsAlreadyExistsButWithDifferentId(fish.getName() , fishId);
+    }
+
+
+    private void throwExceptionIfFishNameIsAlreadyExistsButWithDifferentId(String name , Long fishId) {
+
+        Optional<Fish> fish = fishRepository.findByName(name);
+
+        if (fish.isPresent() && !fish.get().getId().equals(fishId)) {
+            throw new AlreadyExistsException("Fish with name " + name + " already exists");
+        }
+    }
+
+    private void throwExceptionIfFishNameIsAlreadyExists(String name) {
+
+        Optional<Fish> fish = fishRepository.findByName(name);
+
+        if (fish.isPresent()) {
+            throw new AlreadyExistsException("Fish with name " + name + " already exists");
+        }
+
+    }
+}
