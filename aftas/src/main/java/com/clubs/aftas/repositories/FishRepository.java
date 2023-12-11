cfdacc55bed96017cd4b061e9f986edcfb6db87e
saadMeddiche <saadmeddiche2004201@gmail.com>
2023-12-11 11:19:46 +0100

    create FishRepository.findByName()

--- aftas/src/main/java/com/clubs/aftas/repositories/FishRepository.java
+++ aftas/src/main/java/com/clubs/aftas/repositories/FishRepository.java
@@ -3,5 +3,9 @@ package com.clubs.aftas.repositories;
 import com.clubs.aftas.entities.Fish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FishRepository extends JpaRepository<Fish, Long> {
+
+    Optional<Fish> findByName(String name);
 }
